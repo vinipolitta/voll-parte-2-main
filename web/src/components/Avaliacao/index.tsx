4d94cce8a09c141ef377150e34cd6047f82f5fbd
vinipolitta <vinicius.politta1@gmail.com>
2024-08-21 23:03:42 -0300

    feat: preparando o projeto

--- web/src/components/Avaliacao/index.tsx
+++ web/src/components/Avaliacao/index.tsx
@@ -4,29 +4,30 @@ import Botao from "../Botao";
 import Card from "./Card";
 
 const SecaoCard = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  width: 100%;
-`;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+width: 100%;
+`
 
-function Avaliacao({
-  profissionais,
-}: {
-  profissionais: IProfissional[] | null;
-}) {
-  return (
-    <>
-      <SecaoCard>
-        {profissionais && profissionais.length
-          ? profissionais?.map((profissional) => {
-              return <Card profissional={profissional} />;
-            })
-          : "Nao ha avaliacoes para mostrar"}
-      </SecaoCard>
-      {profissionais && profissionais.length > 4 ? <Botao>Ver mais</Botao> : ""}
-    </>
-  );
+function Avaliacao({ profissionais }: { profissionais: IProfissional[] | null }) {
+    return (
+        <>
+            <SecaoCard>
+                {profissionais && profissionais.length 
+                ? 
+                profissionais?.map((profissional) => {
+                  return <Card profissional={profissional} />
+                } )
+                : 
+                "Não há avaliações para mostrar"}
+            </SecaoCard>
+{profissionais && profissionais.length > 4 ?
+            <Botao>Ver mais</Botao>
+        : ""
+        }
+        </>
+    )
 }
 
 export default Avaliacao;
