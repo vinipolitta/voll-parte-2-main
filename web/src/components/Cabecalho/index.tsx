999b39eff1f552bbf863ffe4d302f4f01877ce08
vinipolitta <vinicius.politta1@gmail.com>
2024-09-03 09:00:05 -0300

    feat: cadastro de profissionais

--- web/src/components/Cabecalho/index.tsx
+++ web/src/components/Cabecalho/index.tsx
@@ -1,33 +1,34 @@
-import styled from "styled-components";
-import logo from "./assets/logo.png";
-import perfil from "./assets/perfil.png";
-import pesquisa from "./assets/search.png";
-import autenticaStore from "../../stores/autentica.store";
+import styled from 'styled-components';
+import logo from './assets/logo.png';
+import perfil from './assets/perfil.png';
+import pesquisa from './assets/search.png';
+import autenticaStore from '../../stores/autentica.store';
+
 
 const CabecalhoEstilizado = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 2em 4em;
-`;
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2em 4em
+`
 
 const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  flex-grow: 0.1;
-`;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-grow: .1;
+`
 
 const LinkEstilizado = styled.a`
-  color: var(--azul-escuro);
-  font-weight: 700;
-`;
+ color: var(--azul-escuro);
+ font-weight: 700;
+`
 
 const LinkEstilizadoDeslogado = styled(LinkEstilizado)`
-  font-weight: 400;
-  text-decoration: none;
-  color: var(--azul-escuro);
-`;
+font-weight: 400;
+text-decoration: none;
+color: var(--azul-escuro)
+`
 
 const ContainerPesquisa = styled.div`
   display: flex;
@@ -49,51 +50,45 @@ const SpanCustomizado = styled.span`
   background-repeat: no-repeat;
   width: 25px;
   height: 25px;
-  background-position: 10px;
+background-position: 10px;
 `;
 
 const BotaoEstilizado = styled.a`
-  background-color: var(--azul-escuro);
-  border-radius: 8px;
-  padding: 12px 16px;
-  color: var(--branco);
-  text-decoration: none;
-`;
+background-color: var(--azul-escuro);
+border-radius: 8px;
+padding: 12px 16px;
+color: var(--branco);
+text-decoration: none;
+`
+
 
 function Cabecalho() {
-  const handleLogout = () => {
-    autenticaStore.logout();
-  };
+    const handleLogout = () => {
+        autenticaStore.logout();
+    };
 
-  return (
-    <CabecalhoEstilizado>
-      <img src={logo} alt="logo da empresa Voll" />
-      <Container>
-        {autenticaStore.estaAutenticado ? (
-          <>
-            <img src={perfil} alt="imagem de perfil do usuário" />
-            <LinkEstilizado href="/" onClick={handleLogout}>
-              Sair
-            </LinkEstilizado>
-          </>
-        ) : (
-          <>
-            <LinkEstilizadoDeslogado href="/sobre">
-              Sobre
-            </LinkEstilizadoDeslogado>
-            <LinkEstilizadoDeslogado href="/cadastro">
-              Cadastre-se
-            </LinkEstilizadoDeslogado>
-            <ContainerPesquisa>
-              <InputCustomizado type="text" placeholder="Digite sua busca" />
-              <SpanCustomizado />
-            </ContainerPesquisa>
-            <BotaoEstilizado href="/login">Entrar</BotaoEstilizado>
-          </>
-        )}
-      </Container>
-    </CabecalhoEstilizado>
-  );
+    return (
+        <CabecalhoEstilizado>
+            <img src={logo} alt="logo da empresa Voll" />
+            <Container>
+                {autenticaStore.estaAutenticado
+                    ? <>
+                        <img src={perfil} alt="imagem de perfil do usuário" />
+                        <LinkEstilizado href="/" onClick={handleLogout}>Sair</LinkEstilizado>
+                    </>
+                    : <>
+                        <LinkEstilizadoDeslogado href="/sobre" >Sobre</LinkEstilizadoDeslogado>
+                        <LinkEstilizadoDeslogado href="/cadastro">Cadastre-se</LinkEstilizadoDeslogado>
+                        <ContainerPesquisa>
+                            <InputCustomizado type="text" placeholder='Digite sua busca' />
+                            <SpanCustomizado />
+                        </ContainerPesquisa>
+                        <BotaoEstilizado href="/login">Entrar</BotaoEstilizado>
+                    </>
+                }
+            </Container>
+        </CabecalhoEstilizado>
+    )
 }
 
 export default Cabecalho;
--- web/src/utils/RotaPrivada.tsx
+++ web/src/utils/RotaPrivada.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
-import autenticaStore from "../stores/autentica.store";
+import autenticaStore from "../stores/autentica.store"
 
 const RotaPrivada = () => {
-  const { estaAutenticado } = autenticaStore;
+    const {estaAutenticado} = autenticaStore;
 
-  return estaAutenticado ? <Outlet /> : <Navigate to="/login" />;
-};
+    return (
+        estaAutenticado ? <Outlet /> : <Navigate to="/login" />
+    )
+}
 
 export default RotaPrivada;
